3f1e88801fc4c2bd885ff0fc739b7da786aa20e3
Doyeon Kim <kdy0902ysh@gmail.com>
2024-04-09 11:23:38 +0900

    Title: 나무박멸, Time: 408ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240409/나무박멸/tree-kill-all.py
@@ -0,0 +1,62 @@
+import copy
+n,m,kk,c = list(map(int, input().split()))
+board = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == -1:
+            board[i][j] = -999999
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+vx = [1,1,-1,-1] # 대각선
+vy = [1,-1,1,-1]
+ans = 0
+
+for _ in range(m):
+    # 1. 나무의 성장
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > 0:
+                tmp = 0
+                for k in range(4):
+                    if 0<=i+dx[k]<n and 0<=j+dy[k]<n and board[i+dx[k]][j+dy[k]] > 0:
+                        tmp += 1
+                board[i][j] += tmp
+            if board[i][j]<0:
+                board[i][j] += 1
+    # 2. 나무의 번식
+    new_board = copy.deepcopy(board)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > 0:
+                tmp = []
+                for k in range(4):
+                    if 0<=i+dx[k]<n and 0<=j+dy[k]<n and board[i+dx[k]][j+dy[k]] == 0:
+                        tmp.append([i+dx[k],j+dy[k]])
+                if len(tmp)>=1:
+                    for k in tmp:
+                        new_board[k[0]][k[1]] += board[i][j] // len(tmp)
+    board = new_board
+    # 3. 제초제
+    die_tree = [0,0,0]
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > -1:
+                die = board[i][j]
+                for k in range(4):
+                    for l in range(kk):
+                        if 0<=i+vx[k]*(l+1)<n and 0<=j+vy[k]*(l+1)<n and board[i+vx[k]*(l+1)][j+vy[k]*(l+1)] > 0:
+                            die += board[i+vx[k]*(l+1)][j+vy[k]*(l+1)]
+                        else:
+                            break
+                if die > die_tree[0]:
+                    die_tree = [die,i,j]
+    ans += die_tree[0]
+    board[die_tree[1]][die_tree[2]] = -c -1
+    for k in range(4):
+        for l in range(kk):
+            if 0<=die_tree[1]+vx[k]*(l+1)<n and 0<=die_tree[2]+vy[k]*(l+1)<n and board[die_tree[1]+vx[k]*(l+1)][die_tree[2]+vy[k]*(l+1)] > -1:
+                board[die_tree[1]+vx[k]*(l+1)][die_tree[2]+vy[k]*(l+1)] = -c-1
+            else:
+                break
+print(ans)
